5fcd8d878280db4d193de07972c62f49bc7b2fed
RSarona <saronaraulen@gmail.com>
2021-06-28 22:32:08 +0800

    Changed some comments

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const User_Infos = require('../models/User_Infos')      //model
-
+//GET DATA FROM DB
 router.get('/', async (req, res) => {
-    //get from db
     try {
-        const infoGet = await User_Infos.find({'country':'QAT'}, {'_id':0,'name':1,'age':1});
+        const infoGet = await User_Infos.find();
         res.json(infoGet);
     }catch {
         res.json({message: err})
     }
 });
+
 // //display
 // router.get('/display', (req, res) => {
 //     res.send('This is a userinfo page to get your info!');
 // });
 
+//SAVE DATA TO DB
 router.post('/', async (req,res) => {
     const info = new User_Infos({               //Uses the model and at the same time get info
         name: req.body.name,
         age: req.body.age,
         country: req.body.country
     });
-    //save to db
     try {
         const savedInfo = await info.save();
         res.json(savedInfo);
