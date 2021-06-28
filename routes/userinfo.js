f66ec089c61f7f207522722eeda2a32fcae9b927
RSarona <saronaraulen@gmail.com>
2021-06-28 19:24:26 +0800

    Changed //save to db and added //get from db
    Used try catch method and added a method to get data from db

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -2,26 +2,33 @@ const express = require('express');
 const router = express.Router();
 const User_Infos = require('../models/User_Infos')      //model
 
-router.get('/', (req, res) => {
-    res.send('This is a userinfo page to get your info!');
+router.get('/', async (req, res) => {
+    //get from db
+    try {
+        const infoGet = await User_Infos.find({'country':'QAT'}, {'_id':0,'name':1,'age':1});
+        res.json(infoGet);
+    }catch {
+        res.json({message: err})
+    }
 });
-//display
-router.get('/display', (req, res) => {
-    res.send('This is a userinfo page to get your info!');
-});
+// //display
+// router.get('/display', (req, res) => {
+//     res.send('This is a userinfo page to get your info!');
+// });
 
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
     const info = new User_Infos({               //Uses the model and at the same time get info
         name: req.body.name,
         age: req.body.age,
-        address: req.body.address
+        country: req.body.country
     });
     //save to db
-    info.save().then(data =>{
-        res.json(data);
-    }).catch(err =>{
-        res.json({message: err});
-    })
+    try {
+        const savedInfo = await info.save();
+        res.json(savedInfo);
+    }catch(err) {
+        res.json( {message: err} );
+    }
 });
 
 module.exports = router;
