eb1ffbaf6bd9162bc2fc1fbbd0b5863b5d9b4462
RSarona <saronaraulen@gmail.com>
2021-06-28 18:40:43 +0800

    Added post feature for the API
    Can now post information (tested with postman)

--- routes/userinfo.js
+++ routes/userinfo.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User_Infos = require('../models/User_Infos')      //model
 
 router.get('/', (req, res) => {
     res.send('This is a userinfo page to get your info!');
@@ -7,6 +8,20 @@ router.get('/', (req, res) => {
 //display
 router.get('/display', (req, res) => {
     res.send('This is a userinfo page to get your info!');
+});
+
+router.post('/', (req,res) => {
+    const info = new User_Infos({               //Uses the model and at the same time get info
+        name: req.body.name,
+        age: req.body.age,
+        address: req.body.address
+    });
+    //save to db
+    info.save().then(data =>{
+        res.json(data);
+    }).catch(err =>{
+        res.json({message: err});
+    })
 });
 
 module.exports = router;
